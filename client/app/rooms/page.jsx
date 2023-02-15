04abbb26686f1bbc580a1fe2bfaed9c6ffadc7b5
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-02-15 10:22:29 +0100

    Room page initiation

---
+++ client/app/rooms/page.jsx
@@ -0,0 +1,15 @@
+import HeaderFuncitons from '../(componets)/HeaderFunctions'
+import VideoStreaming from '../(componets)/VideoStreaming'
+import ParticipantChatBox from '../(componets)/ParticipantChatBox'
+import BottomFunctions from '../(componets)/BottomFunctions'
+
+export default function page() {
+  return (
+    <>
+        <HeaderFuncitons />
+        <VideoStreaming />
+        <ParticipantChatBox />
+        <BottomFunctions />
+    </>
+  )
+}
