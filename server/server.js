f5125048fee984fd595acd6b32a522327e53b429
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-02-19 15:11:50 +0100

    Added database....

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
-
 const express = require('express')
 const app = express()
 
+// DATABASE CODE
+const mongoose = require('mongoose')
+// const Userdb = require('./userschema')
+// const Roomdb = require('./roomschema')
+mongoose.connect('mongodb://127.0.0.1/usersdb', () => {console.log('db connected')}, err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/availableRooms', (rq, rs) => {
+  rs.json({d: 'Hello World!'})
 })
 
 app.listen(3001, () => console.log(`Example app listening on port ${3001}`))
